a307f65007f97c5d72bacc942b5102e3504b00aa
hassamburq <hassam@burqup.com>
2025-07-12 23:59:55 -0400

    updated text fields

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,25 @@
-export default function Player({ name, symbol }) {
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+
+  const handleEditClick = () => setIsEditing((editing) => !editing);
+  const handleChange = (event) => setPlayerName(event.target.value);
+
+  let playerNameField = <span className="player-name">{playerName}</span>;
+  if (isEditing)
+    playerNameField = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {playerNameField}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
